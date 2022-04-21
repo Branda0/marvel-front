2609ba7ba71a748ebd5d5606d6eebd27fae733ad
Branda0 <g.brandao.dev@gmail.com>
2022-04-21 21:05:59 +0200

    working on adding burger menu

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,25 +1,42 @@
+import { useState } from "react";
 import logoHeader from "../assets/img/MarvelLogo.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const Header = ({ isLogged, setTokens, setSignupModal, setLoginModal }) => {
+  const [openedMenu, setOpenedMenu] = useState(false);
+
   return (
     <div className="header container">
       <img src={logoHeader} alt="" />
       <div className="right-header">
-        {isLogged ? (
-          <button
-            onClick={() => {
-              setTokens(null);
-            }}
-            className="disconnect-btn"
-          >
-            SIGN OUT
-          </button>
-        ) : (
-          <div className="loggin-signup-btn-container ">
-            <button onClick={() => setSignupModal(true)}>SIGN UP</button>
-            <button onClick={() => setLoginModal(true)}>LOGIN</button>
-          </div>
-        )}
+        <div className="desktop-menu">
+          {isLogged ? (
+            <button
+              onClick={() => {
+                setTokens(null);
+              }}
+              className="disconnect-btn"
+            >
+              SIGN OUT
+            </button>
+          ) : (
+            <>
+              <div className="loggin-signup-btn-container ">
+                <button onClick={() => setSignupModal(true)}>SIGN UP</button>
+                <button onClick={() => setLoginModal(true)}>LOGIN</button>
+              </div>
+            </>
+          )}
+        </div>
+        <div
+          className={`burger-menu ${openedMenu ? "opened" : ""}`}
+          onClick={() => setOpenedMenu(!openedMenu)}
+        >
+          <div></div>
+          <div></div>
+          <div></div>
+          {/* <FontAwesomeIcon icon="fa-solid fa-bars" /> */}
+        </div>
       </div>
     </div>
   );
