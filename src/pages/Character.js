51ad573458a7601b298a5c91836b7895aa919990
Branda0 <g.brandao.dev@gmail.com>
2022-04-27 14:16:41 +0200

    character page responsive

--- src/pages/Character.js
+++ src/pages/Character.js
@@ -30,47 +30,40 @@ const Character = ({ comicsFavorites, setComicsFavorites }) => {
     fetchData();
   }, [id]);
 
-  console.log(data);
-
   return isLoading ? (
     <Loading />
   ) : (
-    <div className="character">
-      <div className="container">
-        <div className="character-infos">
-          <div className="character-img-container">
-            <img src={`${data.thumbnail.path}.${data.thumbnail.extension}`} alt="character" />
-          </div>
-          <div className="character-text-container">
-            <h1>{data.name.toUpperCase()}</h1>
-            <p>{data.description}</p>
-          </div>
+    <div className="character container">
+      <div className="character-infos">
+        <img src={`${data.thumbnail.path}.${data.thumbnail.extension}`} alt="character" />
+        <div className="character-text-container">
+          <h1>{data.name.toUpperCase()}</h1>
+          <p>{data.description}</p>
         </div>
-        <span>Featured in ...</span>
-        {data.comics.length > 0 && (
-          <>
-            <div className="comics-thumbnails">
-              {data.comics &&
-                data.comics.map((comic) => {
-                  if (
-                    comic.thumbnail.path !== "http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available"
-                  ) {
-                    return (
-                      <ComicCard
-                        key={comic._id}
-                        comic={comic}
-                        comicsFavorites={comicsFavorites}
-                        setComicsFavorites={setComicsFavorites}
-                      />
-                    );
-                  } else {
-                    return <></>;
-                  }
-                })}
-            </div>
-          </>
-        )}
       </div>
+
+      {data.comics.length > 0 && (
+        <>
+          <span>Featured in ...</span>
+          <div className="comics-thumbnails">
+            {data.comics &&
+              data.comics.map((comic) => {
+                if (
+                  comic.thumbnail.path !== "http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available"
+                ) {
+                  return (
+                    <ComicCard
+                      key={comic._id}
+                      comic={comic}
+                      comicsFavorites={comicsFavorites}
+                      setComicsFavorites={setComicsFavorites}
+                    />
+                  );
+                }
+              })}
+          </div>
+        </>
+      )}
     </div>
   );
 };
